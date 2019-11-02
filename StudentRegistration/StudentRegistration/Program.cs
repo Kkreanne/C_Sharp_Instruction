c44aba6642d435b99d66e61416323eecdede5a4c
Kkreanne <54691061+Kkreanne@users.noreply.github.com>
2019-11-02 19:56:01 -0400

    Student Registration in C#

---
+++ StudentRegistration/StudentRegistration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StudentRegistration {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Student Registration Form\n");
+
+            Console.WriteLine("Enter First Name: ");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter year of birth: ");
+            string BirthYear = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + FirstName + " " + LastName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: " + FirstName + "*" + BirthYear);
+
+            Console.WriteLine("Goodbye!");
+
+        }
+    }
+}
