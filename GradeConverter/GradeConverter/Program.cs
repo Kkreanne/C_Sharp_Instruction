6e3cdfab30c785c1c23fca0ea6fe05eae34df5b1
Kkreanne <54691061+Kkreanne@users.noreply.github.com>
2019-11-03 18:06:46 -0500

    Grade Converter in C#

---
+++ GradeConverter/GradeConverter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GradeConverter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Letter Grade Converter!");
+
+            string choice = "y";
+            while (choice.Equals("y")) {
+
+                Console.WriteLine("Enter Numerical Grade: ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+
+                if (grade >= 90 && grade <= 100) {
+                    Console.WriteLine("Letter Grade is: A ");
+                } else if (grade >= 80 && grade <= 89) {
+                    Console.WriteLine("Letter Grade is: B ");
+                } else if (grade >= 70 && grade <= 79) {
+                    Console.WriteLine("Letter Grade is: C ");
+                } else if (grade >= 60 && grade <= 69) {
+                    Console.WriteLine("Letter Grade is: D ");
+                } else if (grade >= 50 && grade <= 59) {
+                    Console.WriteLine("Letter Grade is: F ");
+                } 
+                
+                Console.WriteLine("Continue? y/n");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
